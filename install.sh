abcd7b394ae6eb85949b9d5b2a31d151506a218d
Ubuntu <ubuntu@ip-172-31-47-24.eu-west-3.compute.internal>
2020-04-02 12:55:02 +0000

    add vimrc and mosh, must clean correctly

--- install.sh
+++ install.sh
@@ -6,3 +6,11 @@ then
 fi
 
 cp config $config_file
+
+# Install vimrc configuration
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Install mosh server (only works with ubuntu now)
+sudo apt update
+sudo apt install mosh
